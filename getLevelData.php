9cb91188c0b9e9f407b7be4d63e36cf9ac1a86ec
kardoj <kardoj@gmail.com>
2015-09-19 19:30:19 +0300

    0.1
    first iteration of level editor, shape dragging, level data fetching

---
+++ getLevelData.php
@@ -0,0 +1,51 @@
+﻿<?php
+	
+	// An object containing border, pieces array and array of pieces' coordinates if available
+	if(isSet($_REQUEST["level"])){
+		$level = $_REQUEST["level"];
+		$response = new StdClass();
+		
+		$level_resources_path = "levels/" . $level;
+		
+		// Every level must have border.png
+		$border = new StdClass();
+		$border->image = $level_resources_path . "/border.png";
+		$border_info = getimagesize($level_resources_path . "/border.png");
+		$border->width = $border_info[0];
+		$border->height = $border_info[1];
+		$response->border = $border;
+		
+		$pieces = array();
+		foreach(scandir($level_resources_path) as $piece){
+			if(strpos($piece, ".png") != false && $piece != "border.png"){
+				$piece_info = new StdClass();
+				$piece_info->image = $level_resources_path . "/" . $piece;
+				$size_data = getimagesize($level_resources_path . "/" . $piece);
+				$piece_info->width = $size_data[0];
+				$piece_info->height = $size_data[1];
+				array_push($pieces, $piece_info);
+			}
+		}
+		$response->pieces = $pieces;
+		$response->coordinates = null;
+		
+		// If image positions file exists, get its contents
+		if(file_exists($level_resources_path . "/coordinates.txt")){
+			$coordinates = file_get_contents($level_resources_path . "/coordinates.txt");
+			$file_rows = explode("\n", $coordinates);
+			$coordinates_array = array();
+			foreach($file_rows as $row){
+				$row_pieces = explode(" ", $row);
+				$one = new StdClass();
+				$one->image = trim($row_pieces[0]);
+				$one->x = trim($row_pieces[1]);
+				$one->y = trim($row_pieces[2]);
+				array_push($coordinates_array, $one);
+			}
+			$response->coordinates = $coordinates_array;
+		}
+		
+		echo json_encode($response);
+	}
+	
+?>
---
+++ setup.php
@@ -0,0 +1,112 @@
+﻿<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Tasemete ülesseadmine</title>
+	<script src="http://code.jquery.com/jquery-2.1.4.min.js"></script>
+	<link rel="stylesheet" href="style.css">
+	<script>
+		var draggedPiece = null;
+		// Mouse relation to the dragged object while dragging (to calculate landing)
+		var mouseToDragged = {
+			x: 0,
+			y: 0
+		};
+		$(document).ready(function(){
+			$("#levels").on("change", function(){
+				var selected = $("#levels option:selected").text();
+				// Remove all the event handlers before removing objects
+				$(document).off();
+				$("#content").empty();
+				if(selected != "Vali tase"){
+					// Retrieve level resources
+					$.ajax({
+						url: "getLevelData.php",
+						data: {level: selected}
+					}).done(function(data){
+						loadLevel(JSON.parse(data));
+					});	
+				}
+			});
+		});
+		
+		function loadLevel(levelData){
+			$("<div id='border'><img src='"+levelData.border.image+"' alt='game border image'></div>").appendTo("#content");
+			$("#border").css("margin", "auto");
+			$("#border").css("left", "0");
+			$("#border").css("right", "0");
+			$("#border").css("top", "0");
+			$("#border").css("bottom", "0");
+			$("#border").css("width", levelData.border.width);
+			$("#border").css("height", levelData.border.height);
+			
+			var borderPosition = $("#border").offset();
+			var borderX = borderPosition.left;
+			var borderY = borderPosition.top;
+			if(levelData.coordinates === null){
+				// Randomly distribute pieces
+				var containerWidth = $("#content").width();
+				var containerHeight = $("#content").height();
+				for(var i=0; i<levelData.pieces.length; i++){
+					var x = Math.ceil(Math.random()*(containerWidth-levelData.pieces[i].width));
+					var y = Math.ceil(Math.random()*(containerHeight-levelData.pieces[i].height));
+					console.log(containerWidth);
+					createPiece(i, levelData.pieces[i].image, x, y);
+				}				
+			} else {
+				// Set pieces to their coordinates
+				for(var i=0; i<levelData.pieces.length; i++){
+					var x = parseInt(borderX) + parseInt(levelData.coordinates[i].x);
+					var y = parseInt(borderY) + parseInt(levelData.coordinates[i].y);					
+					createPiece(i, levelData.pieces[i].image, x, y);
+				}
+			}
+		}
+		
+		// Counter is from the loop and is used to make ids for the pieces (piece1, piece2, etc)
+		function createPiece(counter, imgSrc, x, y){
+			var id = "piece" + (counter+1);
+			$("<div class='piece' id='"+id+"'><img src='"+imgSrc+"' alt='game piece image'></div>").appendTo("#content");
+			var hash_id = "#" + id;
+			$(hash_id).css("left", x);
+			$(hash_id).css("top", y);
+			
+			$(hash_id).on("dragstart", function(){
+				draggedPiece = "#" + $(this).attr("id");
+				var piecePosition = $(draggedPiece).position();
+				mouseToDragged = {
+					x: event.pageX - piecePosition.left,
+					y: event.pageY - piecePosition.top
+				};
+			});
+			
+			$(hash_id).on("drag", function(){
+				if(event.pageX != 0){ // For some reason, last drag event gives 0 for event.pageX
+					var x = event.pageX - mouseToDragged.x;
+					var y = event.pageY - mouseToDragged.y;
+					$(draggedPiece).css("left", x);
+					$(draggedPiece).css("top", y);
+				}
+			});
+		}
+	</script>
+</head>
+<body>
+	<?php
+		$levels = scandir("levels");
+	?>
+	<select name="levels" id="levels">
+		<option value="none" selected="selected">Vali tase</option>
+		<?php 
+			foreach($levels as $level){
+				if($level != "." && $level != ".."){
+					echo "<option value='$level'>$level</option>";
+				}
+			}
+		?>
+	</select>
+	
+	<div id="content">
+	</div>
+</body>
+</html>
